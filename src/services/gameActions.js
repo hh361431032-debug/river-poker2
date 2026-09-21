34c754fe50bc16b8b32658fca4b45672b21fcee3
hh361431032-debug <hh361431032@gmail.com>
2026-09-21 21:36:03 +0800

    feat: add trusted lobby room actions

--- src/services/gameActions.js
+++ src/services/gameActions.js
@@ -21,6 +21,8 @@ export async function gameAction(payload) {
 export const pokerActions = {
   listRooms: () => gameAction({ action: "list_rooms" }),
 
+  listRooms: () => gameAction({ action: "list_rooms" }),
+
   getRoom: (roomCode, username, playerToken = "") =>
     gameAction({ action: "get_room", roomCode, username, playerToken }),
 
@@ -62,6 +64,8 @@ export const pokerActions = {
 
   tick: (roomCode, username, playerToken) =>
     gameAction({ action: "tick", roomCode, username, playerToken }),
+
+  deleteRoom: (roomCode, username) => gameAction({ action: "delete_room", roomCode, username }),
 
   deleteRoom: (roomCode, username) =>
     gameAction({ action: "delete_room", roomCode, username }),
